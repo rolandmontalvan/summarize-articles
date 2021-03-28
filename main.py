83c247e7012b9ff50ca9fe47d045aaabea512bdc
rolandmontalvan <rolandfilho@gmail.com>
2021-03-27 23:36:26 -0300

    first_step

---
+++ main.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+import nltk
+from textblob import TextBlob
+from newspaper import article
+
+url = 'https://edition.cnn.com/2020/09/13/tech/microsoft-tiktok-bytedance/index.html'
+
+article = Article(url)
+
+article.download()
+article.parse()
+article.nlp()
+
+print(f'Title: {article.title}')
+print(f'Authors: {article.authors}')
+print(f'Publication Date: {article.publish_date}')
+print(f'Sommary: {article.summary}')
